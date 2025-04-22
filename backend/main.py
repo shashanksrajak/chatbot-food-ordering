8916a0723609fcfb74347db672edc2bbf5c3445a
Shashank Rajak <shashanksrajak@gmail.com>
2025-04-22 09:05:55 +0530

    Add new conversations in chatbot backend for cart, remove items

--- backend/main.py
+++ backend/main.py
@@ -103,6 +103,12 @@ async def handle_request(request: Request, session: SessionDep):
         case "order_complete_context: ongoing_order":
             print("Completing the order...")
             return complete_order(session_id)
+        case "order.remove_context: ongoing_order":
+            print("Removing an item")
+            return remove_order(parameters, session_id)
+        case "cart.show_context: ongoing-tracking":
+            print("Showing cart...")
+            return show_cart(session_id)
 
         case _:
             return JSONResponse(content={
@@ -137,18 +143,87 @@ def add_order(parameters: dict, session_id: str, session: SessionDep):
         # add orders in cache
         new_food_dict = dict(zip(food_items, quantities))
 
+        # update existing order
         if session_id in in_progress_orders:
             current_order: dict = in_progress_orders[session_id]
-            current_order.update(new_food_dict)
+            # current_order.update(new_food_dict)
+            for food_item, food_quantity in new_food_dict.items():
+                if food_item in current_order:
+                    current_order[food_item] += food_quantity
+                else:
+                    current_order[food_item] = food_quantity
+
+        # create new order
         else:
             in_progress_orders[session_id] = new_food_dict
 
         print(in_progress_orders)
 
+        items_added = [f"{item} (x{int(qty)})" for item,
+                       qty in in_progress_orders[session_id].items()]
+
         # TODO: add a string response with food items and qty
         return JSONResponse(content={
-            "fulfillmentText": f"Added items. Anything else? If you are done you can say done and I will place your order."
+            "fulfillmentText": f"Added items. Anything else? If you are done you can say done and I will place your order. So far you have added - {", ".join(items_added)}."
 
+        })
+
+
+def remove_order(parameters: dict, session_id: str):
+    food_items = parameters["food_item"]
+
+    not_existing_items = []
+    removed_items = []
+    # update existing order
+    if session_id in in_progress_orders:
+        current_order: dict = in_progress_orders[session_id]
+        # current_order.update(new_food_dict)
+        for food_item in food_items:
+            if food_item in current_order:
+                del current_order[food_item]
+                removed_items.append(food_item)
+            else:
+                not_existing_items.append(food_item)
+        if not current_order:
+            del in_progress_orders[session_id]
+
+    else:
+        return JSONResponse(content={
+            "fulfillmentText": f"oops! Seems like your order does not exist. Please say New Order to start placing a new order."
+
+        })
+
+    print(in_progress_orders)
+
+    removed_text = f"I have removed {', '.join(removed_items)}" if removed_items else "Nothing was removed"
+    not_found_text = f"I could not find {', '.join(not_existing_items)}" if not_existing_items else ""
+
+    fulfillment_text = f"{removed_text}. {not_found_text}. Do you want to add anything else? If you are done you can say done and I will place your order."
+    # Clean up the fulfillment text
+    fulfillment_text = fulfillment_text.replace(". .", ".").strip()
+    if fulfillment_text.startswith("I have removed Nothing was removed. "):
+        fulfillment_text = fulfillment_text[len(
+            "I have removed Nothing was removed. "):].lstrip(". ")
+
+    return JSONResponse(content={
+        "fulfillmentText": fulfillment_text
+    })
+
+
+def show_cart(session_id):
+    """
+    Returns back the cart items
+    """
+    if session_id in in_progress_orders:
+        items_added = [f"{item} (x{int(qty)})" for item,
+                       qty in in_progress_orders[session_id].items()]
+        fulfillment_text = f"You have added these items in your cart: {", ".join(items_added)}"
+        return JSONResponse(content={
+            "fulfillmentText": fulfillment_text
+        })
+    else:
+        return JSONResponse(content={
+            "fulfillmentText": "oops! Your cart looks empty. Please add items by saying New Order."
         })
 
 
@@ -167,17 +242,13 @@ def track_order(parameters: dict, session: SessionDep):
                 "fulfillmentText": f"Sorry this Order Id is invalid. Please provide correct Order Id (e.g. 234567)"
             })
         return JSONResponse(content={
-            "fulfillmentText": f"Your order status is {order.status}"
+            "fulfillmentText": f"Your order is {order.status}. Please keep patience, our chefs are preparing delicious food for you. Thanks for availing our services."
         })
     except Exception as e:
         print(e)
         return JSONResponse(content={
             "fulfillmentText": f"Invalid Order ID. Please provide correct Order ID."
         })
-
-
-def remove_order():
-    pass
 
 
 def complete_order(session_id: str):
@@ -191,27 +262,38 @@ def complete_order(session_id: str):
         print("order_id", order_id)
         print("order", order)
 
+        ordered_item_details = []
+        total_bill = 0
+
         with Session(engine) as session:
             for food_item, quantity in order.items():
                 food_item_record = session.exec(
                     select(FoodItem).where(FoodItem.name.ilike(f"%{food_item}%"))).first()
-            if not food_item_record:
-                fulfillmentText = f"Sorry, we don't have {food_item} in our menu."
-                return JSONResponse(content={"fulfillmentText": fulfillmentText})
+                if not food_item_record:
+                    fulfillmentText = f"Sorry, we don't have {food_item} in our menu."
+                    return JSONResponse(content={"fulfillmentText": fulfillmentText})
+                else:
+                    price = food_item_record.price
+                    total_price = price * quantity
+                    total_bill += total_price
 
-            new_order = Order(
-                order_id=order_id,
-                food_item_id=food_item_record.id,
-                status="Preparing",
-                quantity=quantity,
-                total_price=food_item_record.price * quantity,
-                created_at=date.today()
-            )
-            session.add(new_order)
+                    new_order = Order(
+                        order_id=order_id,
+                        food_item_id=food_item_record.id,
+                        status="Preparing in Kitchen",
+                        quantity=quantity,
+                        total_price=total_price,
+                        created_at=date.today()
+                    )
+                    session.add(new_order)
+                    ordered_item_details.append(
+                        f"{food_item} (x{int(quantity)})")
+
             del in_progress_orders[session_id]
 
             session.commit()
-            fulfillmentText = f"Your order has been placed successfully. Thank you! Here is your Order ID # {order_id}"
+
+            fulfillmentText = f"Your order has been placed successfully. Thank you! Here is your Order ID # {order_id}. Your total bill amount is ₹{total_bill:.2f} and you have ordered: {', '.join(ordered_item_details)}."
 
     return JSONResponse(content={
         "fulfillmentText": fulfillmentText
@@ -221,6 +303,7 @@ def complete_order(session_id: str):
 def main():
     print("Hello from backend!")
     uvicorn.run(app, host="0.0.0.0", port=9001)
+    # uvicorn.run(app, host="::", port=9001)
 
 
 if __name__ == "__main__":
